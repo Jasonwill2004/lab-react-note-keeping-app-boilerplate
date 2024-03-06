c752a6c07ededba5e3634af3faef61c03b0e1092
jason william <jasonwilliam@jasons-MacBook-Air.local>
2024-03-06 11:17:28 +0530

    done

---
+++ pronote/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Edit from './components/Edit';
+// import Editor from './components/Edit';
+
+function App() {
+  return (
+    <div className="App">
+      <Edit/>
+    </div>
+  );
+}
+
+export default App;
